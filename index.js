754e360bac6416d33eab2d1a71f2387d7844a336
The Doctor <code.doc113@gmail.com>
2023-10-01 16:33:05 +0600

    made the code look nice, added js, fixed dropdown menu and more.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function goToNewPAge(){
-    let url = document.getElementById(pageSelection).value;
+    let url = document.getElementById("pageSelection").value;
     if (url != "none"){
         window.location = url;
     }
