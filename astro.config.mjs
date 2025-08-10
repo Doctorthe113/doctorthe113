7e829b78ae2b54febbd7ab749f8f52f28e4a177e
doctorthe113 <code.doc113@gmail.com>
2025-08-11 01:14:33 +0600

    added github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,28 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+const script = process.env.npm_lifecycle_script || "";
+const isBuild = script.includes("build");
+
+let siteUrl;
+if (isBuild) {
+    siteUrl = "https://doctorthe113.com";
+} else {
+    siteUrl = "http://localhost:3000";
+}
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-	},
-
-  integrations: [react()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [react()],
+    site: siteUrl,
+    server: {
+        port: 3000,
+    },
 });
---
+++ src/pages/blogs/sample-post.md
@@ -0,0 +1,9 @@
+---
+title: "Sample Post"
+author: "Doctorthe113"
+date: "2025-08-08"
+description: "This is a sample post"
+layout: ../../layouts/BlogBaseLayout.astro
+---
+
+This is a sample blog post. Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nesciunt voluptas optio fugiat odit doloremque saepe, pariatur possimus, veritatis provident maiores sunt excepturi sapiente quisquam! Animi excepturi odio perspiciatis tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nesciunt voluptas optio fugiat odit doloremque saepe, pariatur possimus, veritatis provident maiores sunt excepturi sapiente quisquam! Animi excepturi odio perspiciatis tenetur?
